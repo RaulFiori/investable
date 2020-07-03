ce24ce3ab18d13ab8f26807ca5f548c1fd43766e
Raul Fiori <raulproc1807@gmail.com>
2020-07-02 22:16:38 -0300

    added initial dependencies, configured linter

---
+++ src/App.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Background = styled.main`
+  background: transparent radial-gradient(closest-side at 50% 50%, #403850 0%, #30283d 100%) 0% 0% no-repeat padding-box;
+  opacity: 1;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 0px 82px;
+  height: 100vh;
+`;
